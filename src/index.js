33450908505922a80b5bcf8daf6dd202b6a221a2
vmorenomarin <vmorenomarin@gmail.com>
2021-10-04 22:55:18 -0500

    Creación de estructura de directorios y inicialización con librerías básicas del sistema.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = requires("express");
+const morgan = requires("morgan");
+const cors = requires("cors");
+
+const app = express();
+
+app.set("Port", 4000);
+
+app.use(morgan("dev")); // Permite lka creación de un log de las peticiones realizadas hacia el servidor y de las respuestas.
+
+app.use(express.urlencoded({ extended: true })); // necesaria para poder recibir datos desde el front end.
+
+app.use(express.json()); // Necesaria para la conversión de datos en json y leerlos adecuadamente.
+app.use(cors({ origin: "*" })); // Permite las conexiones desde cualquier cliente.
+
+
