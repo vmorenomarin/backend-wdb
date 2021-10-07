eb26234591e67ddb37af8202b114326ce5ea27d6
vmorenomarin <vmorenomarin@gmail.com>
2021-10-06 21:58:36 -0500

    Generación de los controladores para listar usurio por ID, actualizar y borrar, así como la generación de las respectivas rutas de los controladores.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,6 +13,62 @@ userCtrl.list = async (req, res) => {
   }
 };
 
+userCtrl.listid = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await userModel.findById({ _id: id });
+    if (!user) {
+      return res.status(404).json({
+        ok: false,
+        message: "Id number not finded.",
+      });
+    }
+    res.json({ ok: true, message: user });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      message: error.message,
+    });
+  }
+};
+
+userCtrl.update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await userModel.findById({ _id: id });
+    if (!user) {
+      return res.status(404).json({
+        ok: false,
+        message: "Id number not finded.",
+      });
+    }
+
+    /* Las siguientes líneas permitirá evitar errores, que los campos no queden vacíos y que tome el dato que tiene en el documento */
+    const username = req.body.name || user.name;
+    const lastname = req.body.name || user.lastname;
+    const email = req.body.name || user.email;
+    const salary = req.body.salary || user.salary;
+
+    const userUpdate = {
+      username,
+      lastname,
+      email,
+      salary,
+    };
+
+    await user.updateOne(userUpdate);
+    res.json({
+      ok: true,
+      message: "User update.",
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      message: error.message,
+    });
+  }
+};
+
 userCtrl.add = async (req, res) => {
   try {
     const { name, lastname, email, salary } = req.body;
@@ -20,7 +76,7 @@ userCtrl.add = async (req, res) => {
     if (verify) {
       return res.json({
         ok: false,
-        message: "Email direcctio already exists with other user.",
+        message: "Email address already exists relates to other user.",
       });
     }
     const newUser = new userModel({
@@ -35,6 +91,31 @@ userCtrl.add = async (req, res) => {
       ok: true,
       newUser,
     });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      message: error.message,
+    });
+  }
+};
+
+userCtrl.delete = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await userModel.findById({ _id: id });
+    if (!user) {
+      return res.status(404).json({
+        ok: false,
+        message: "Id number not finded.",
+      });
+    }
+
+    await user.deleteOne();
+    res.json({
+      ok: true,
+      message: "User deleted.",
+    });
+    res.json({ ok: true, message: user });
   } catch (error) {
     res.status(500).json({
       ok: false,
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,5 +3,8 @@ const userCtrl = require("../controllers/user.controller");
 const route = Router();
 
 route.get("/", userCtrl.list);
-route.post("/", userCtrl.add)
+route.post("/", userCtrl.add);
+route.get("/userid/:id", userCtrl.listid)
+route.put("/userupdate/:id", userCtrl.update)
+route.delete("/userdelete/:id", userCtrl.delete)
 module.exports = route;
