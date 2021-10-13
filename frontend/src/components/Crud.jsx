a66138a069b693a7b94713ff2ce6a0e09d573348
vmorenomarin <vmorenomarin@gmail.com>
2021-10-12 22:38:15 -0500

    Agregado las demás funciones que sobre el frontend para la eliminiación y actualización de usuarios.

--- frontend/src/components/Crud.jsx
+++ frontend/src/components/Crud.jsx
@@ -1,28 +1,39 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 export const Crud = () => {
   const [users, setUsers] = useState([]);
-  const [input, setInput] = useState([
-    { name: "", lastname: "", email: "", salary: 0 },
-  ]);
+  // const [input, setInput] = useState([
+  //   { name: "", lastname: "", email: "", salary: 0 },
+  // ]);
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [salary, setSalary] = useState(0);
+  const [edit, setEdit] = useState(false);
+
   useEffect(() => {
     getData();
   }, []);
 
   const clearData = () => {
-    setInput([{ name: "", lastname: "", email: "", salary: 0 }]);
+    setName("");
+    setLastname("");
+    setEmail("");
+    setSalary(0);
+    setEdit(false);
   };
 
   const getData = async () => {
     const { data } = await axios.get("http://localhost:4000/");
-    console.log(data.users);
     setUsers(data.users);
   };
 
   const saveUser = async () => {
     try {
-      await axios.post("http://localhost:4000/", input);
+      const newUser = { name, lastname, email, salary };
+      await axios.post("http://localhost:4000/", newUser);
       clearData();
       getData();
     } catch (error) {
@@ -33,9 +44,88 @@ export const Crud = () => {
     }
   };
 
+  const updateUser = async () => {
+    try {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You will modify this user!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, update it!",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          const id = localStorage.getItem("id");
+          console.log(id);
+          const updatedUser = { name, lastname, email, salary };
+          const { data } = await axios.put(
+            "http://localhost:4000/userupdate/" + id,cd
+            updatedUser
+          );
+          clearData();
+          getData();
+          Swal.fire({
+            icon: "success",
+            title: data.message,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+    } catch (error) {
+      if (!error.response.data.ok) {
+        return alert(error.message);
+      }
+      console.log("Error in saveUser", error.message);
+    }
+  };
+
   const actions = (e) => {
     e.preventDefault();
-    saveUser();
+    edit ? updateUser() : saveUser();
+  };
+
+  const restoreDataFields = (item) => {
+    setEdit(true);
+    setName(item.name);
+    setLastname(item.lastname);
+    setEmail(item.email);
+    setSalary(item.salary);
+    localStorage.setItem("id", item._id);
+  };
+
+  const deleteUser = async (id) => {
+    try {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          const { data } = await axios.delete(
+            "http://localhost:4000/userdelete/" + id
+          );
+          getData();
+          clearData();
+          Swal.fire({
+            icon: "success",
+            title: data.message,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+    } catch (error) {
+      if (!error.response.data.ok) {
+        return alert(error.message);
+      }
+      console.log("Error in deleteUser", error.message);
+    }
   };
   return (
     <div className="container">
@@ -50,42 +140,38 @@ export const Crud = () => {
                   type="text"
                   className="form-control mb-2"
                   placeholder="Name"
-                  value={input.name}
+                  value={name}
                   required
-                  onChange={(e) => setInput.name(e.target.value)}
+                  onChange={(e) => setName(e.target.value)}
                 />
                 <input
                   type="text"
                   className="form-control mb-2"
                   placeholder="Lastname"
-                  value={input.lastname}
+                  value={lastname}
                   required
-                  onChange={(e) => setInput.lastname(e.target.value)}
+                  onChange={(e) => setLastname(e.target.value)}
                 />
                 <input
                   type="email"
                   className="form-control mb-2"
                   placeholder="E-mail"
-                  value={input.email}
+                  value={email}
                   required
-                  onChange={(e) => setInput.email(e.target.value)}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
-                <label className="form-label text-muted">Salary:</label>
+                <label className="form-label text-muted ms-2">Salary:</label>
                 <input
                   type="number"
                   className="form-control mb-2"
                   placeholder="Salary"
-                  value={input.salary}
-                  onChange={(e) => setInput.salary(e.target.value)}
+                  value={salary}
+                  onChange={(e) => setSalary(e.target.value)}
                 />
+                <button className="btn btn-primary form-control" type="submit">
+                  Save
+                </button>
               </form>
-              <button
-                className="btn btn-primary form-control"
-                type="submit"
-                onClick={saveUser()}
-              >
-                Save
-              </button>
             </div>
           </div>
         </div>
@@ -113,8 +199,14 @@ export const Crud = () => {
                   <td>{user.email}</td>
                   <td>$ {user.salary}</td>
                   <td className="text-center">
-                    <i className="bi bi-trash btn btn-danger mx-2"></i>
-                    <i className="bi bi-pen btn btn-warning"></i>
+                    <i
+                      className="bi bi-trash btn btn-danger m-2"
+                      onClick={() => deleteUser(user._id)}
+                    ></i>
+                    <i
+                      className="bi bi-pen btn btn-warning"
+                      onClick={() => restoreDataFields(user)}
+                    ></i>
                   </td>
                 </tr>
               ))}
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -44,13 +44,13 @@ userCtrl.update = async (req, res) => {
     }
 
     /* Las siguientes líneas permitirá evitar errores, que los campos no queden vacíos y que tome el dato que tiene en el documento */
-    const username = req.body.name || user.name;
-    const lastname = req.body.name || user.lastname;
-    const email = req.body.name || user.email;
+    const name = req.body.name || user.name;
+    const lastname = req.body.lastname || user.lastname;
+    const email = req.body.email || user.email;
     const salary = req.body.salary || user.salary;
 
     const userUpdate = {
-      username,
+      name,
       lastname,
       email,
       salary,
@@ -90,7 +90,7 @@ userCtrl.add = async (req, res) => {
         message: "E-mail field is required, cannot be empty.",
       });
     }
-    const verify = E - (await userModel.findOne({ email }));
+    const verify = (await userModel.findOne({ email }));
     if (verify) {
       return res.status(400).json({
         ok: false,
